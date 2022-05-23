957e807a3ff96bf8b4f25c74ad8de67c8db9f6aa
andersonstorqui <andersonsstorqui>
2022-05-23 17:24:18 -0300

    login sucess

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import configureStore from './store/index';
 import App from './App';
-import Stepper from 'bs-stepper';
+// import Stepper from 'bs-stepper';
 export const store = configureStore();
 export default store;
 
